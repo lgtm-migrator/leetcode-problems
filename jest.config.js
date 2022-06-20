d0ee6b52b08c376a7eb3f356fd231a6f5a5f53ea
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2022-06-20 20:18:48 +0200

    fix jest test root

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  roots: ['./src'],
   testEnvironment: 'node',
   testRegex: '(/test/.*|(\\.|/)(test|spec))\\.(js|ts)$',
   collectCoverage: true
